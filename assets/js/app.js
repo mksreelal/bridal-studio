233d30f28e9c08a6de784f872a803e43d29a27b7
musadhiq mp <musadhiqmp@gmail.com>
2021-08-07 15:16:08 +0530

    mobile first fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@ window.addEventListener('load' ,function(e){
     const humburger = document.querySelector(".humburgar");
 const navlinks = document.querySelector(".nav-links");
 const overlay = document.querySelector(".overlay");
-console.log(e.target.className)
+const message = document.getElementById("message");
+const button = document.querySelector(".msg");
+
 humburger.addEventListener('click', (e)=>{
     navlinks.classList.toggle('open');
     overlay.classList.toggle('open');
@@ -11,4 +13,10 @@ overlay.addEventListener('click', (e)=>{
     navlinks.classList.remove('open');
     overlay.classList.remove('open');
 });
+
+button.addEventListener('click', ()=>{
+    let messageVal = message.value;
+    button.setAttribute('href',"https://wa.me/+917558936055?text=" + messageVal)
 })
+})
+
